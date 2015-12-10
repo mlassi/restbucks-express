57f5a9392f2c96a837da9229d470706e49cea4f2
Magnus Lassi <magnus.lassi@gmail.com>
2015-12-09 19:34:05 -0500

    cleanup of app.js

--- app/app.js
+++ app/app.js
@@ -1,18 +1,18 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
-var port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 //app.use(bodyParser.urlencoded({extended:true}));
 //app.use(bodyParser.json());
 
-var homeRouter = require('./routes/homeRoutes')();
+const homeRouter = require('./routes/homeRoutes')();
 
 app.use('/api/home', homeRouter);
 
 
 app.listen(port, function(){
-    console.log('Running app on PORT: ' + port);
+    console.log(`Running app on PORT: ${port}`);
 });
 
 module.exports = app;

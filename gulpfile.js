6583ea06b755226351a0e310845d32721b9afed5
Magnus Lassi <magnus.lassi@gmail.com>
2015-12-09 19:27:33 -0500

    added nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,26 @@
 
 var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    nodemon = require('gulp-nodemon');
 
 gulp.task('mocha', function() {
     return gulp.src(['app/**/*.js'], {read:false})
         .pipe(mocha({reporter: 'list'}))
         .on('error', gutil.log);
+});
+
+gulp.task('nodemon', function() {
+   nodemon({script: 'app/app.js',
+            ext: 'js',
+            env: {
+                PORT: 8000
+            },
+            ignore: ['./node_modules/**']
+    })
+    .on('restart', function() {
+        console.log('Restarting');
+    });
 });
 
 gulp.task('watch-mocha', function() {

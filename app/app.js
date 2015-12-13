5a8173b061fc03e8ca3633681009cc4b7d3e87ee
Magnus Lassi <magnus.lassi@gmail.com>
2015-12-12 20:32:55 -0500

    Added bodyparser

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require('express'),
+    bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//app.use(bodyParser.urlencoded({extended:true}));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const homeRouter = require('./routes/homeRoutes')();
 

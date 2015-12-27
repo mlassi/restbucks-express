e117b756b89c9f049d7ec14759ed35ac87c118b5
Magnus Lassi <magnus.lassi@gmail.com>
2015-12-27 17:56:16 -0500

    added price list

--- app/pricing/priceCalculator.js
+++ app/pricing/priceCalculator.js
@@ -1,13 +1,21 @@
+const priceList = require('./priceList')();
+const _ = require('lodash');
+
 const priceCalculator = function () {
 
     function calculate(order) {
-        return order._doc.items.reduce(function(total, item) {
+        return order._doc.items.reduce(function (total, item) {
 
-            if(item._doc.name === 'latte') {
-                total += 2 * item.quantity;
-            }
+            total += findPrice(item) * item.quantity;
+
             return total;
         }, 0);
+    }
+
+    function findPrice(item) {
+        return _(priceList).findWhere(function (priceItem) {
+            return priceItem.name === item._doc.name && priceItem.size === item._doc.size;
+        }).cost;
     }
 
     return {
---
+++ app/pricing/priceList.js
@@ -0,0 +1,24 @@
+const priceList = function() {
+
+    return [
+        {
+            name: "latte",
+            size: "small",
+            cost: 2
+        },
+        {
+            name: "latte",
+            size: "medium",
+            cost: 3
+        },
+        {
+            name: "latte",
+            size: "large",
+            cost: 4
+        }
+    ];
+
+
+}
+
+module.exports = priceList;

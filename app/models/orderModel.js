cf4148499bb4551a4e01df04632493979f3fff7e
Magnus Lassi <magnus.lassi@gmail.com>
2015-12-26 22:02:45 -0500

    updated the order model to allow order items

--- app/models/orderModel.js
+++ app/models/orderModel.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+const itemModel = new Schema({
+    name: {type: String},
+    quantity: {type: Number},
+    size: {type: String}
+});
+
 const orderModel = new Schema({
     location: {type: String},
-    cost: {type: Number},
+    items: [itemModel],
+    cost: {type: Number, default: 0},
     status: {type: String, default: 'unpaid'}
 });
 
